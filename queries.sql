3c546f5329bd3a7d116026c15c2d5a7c4ed44681
weeetttyyyou <113246759+weeetttyyyou@users.noreply.github.com>
2024-05-10 08:14:05 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,38 +1,26 @@
--- Список клиентов, заказавших блюдо дороже 300 рублей:
-SELECT DISTINCT 
-  c.client_id, c.client_name, c.client_surname
-FROM
-  delivery.Clients c
-JOIN
-  delivery.Orders o ON c.client_id = o.client_id
-JOIN
-  delivery.Orders_x_Dishes od ON o.order_id = od.order_id
-WHERE 
-  od.dish_id IN (
-    SELECT
-      dish_id 
-    FROM
-      delivery.Dishes 
-    WHERE
-      dish_price > 300
-  )
-ORDER BY
-  client_id;
+-- Запрос на вывод средней зарплаты по каждой профессии:
+SELECT p.profession_name,
+  AVG(p.profession_salary) AS avg_salary
+FROM 
+  delivery.Professions p
+JOIN 
+  delivery.Staff s ON p.profession_id = s.profession_id
+GROUP BY 
+  p.profession_name;
 
 -- Список наиболее часто покупаемого блюда и общее количество заказов с этим блюдом:
 SELECT
   d.dish_name,
   COUNT(*) AS total_orders
 FROM
-  delivery.Orders_x_Dishes od
+  delivery.Orders_and_Dishes od
 JOIN
-  dekivery.Dishes d ON od.dish_id = d.dish_id
+  delivery.Dishes d ON od.dish_id = d.dish_id
 GROUP BY
   d.dish_name
 ORDER BY
   total_orders DESC
 LIMIT 5;
-
 
 -- Запрос на вывод списка клиентов, оставивших отзывы с оценкой 3:
 SELECT 
@@ -46,43 +34,23 @@ JOIN
 WHERE 
   r.review_rating = 3;
 
--- Запрос на вывод количества заказов, сделанных каждым клиентом за последнюю неделю:
-SELECT 
-  s.client_id,
-  s.client_name,
-  s.client_surname,
-  COUNT(o.order_id) AS total_orders
-FROM 
-  delivery.Clients s
-JOIN 
-  delivery.Orders o ON s.client_id = o.client_id
-WHERE 
-  o.order_date >= CURRENT_DATE - INTERVAL '1 week'
-GROUP BY 
-  s.client_id, s.cliet_name
-ORDER BY 
-  total_orders DESC;
-
--- Средний вес блюд в ресторанах:
-SELECT 
-  d.dish_name,
-  AVG(d.dish_weight) AS avg_dish_weight
+-- Список клиентов, заказавших блюдо дороже 450 рублей:
+SELECT DISTINCT 
+  c.client_id, c.client_name, c.client_surname
 FROM
-  delivery.Dishes d
+  delivery.Clients c
 JOIN
-  delivery.Dishes_x_Restaurants di ON d.dish_id = di.dish_id
-GROUP BY 
-  d.dish_name
+  delivery.Orders o ON c.client_id = o.client_id
+JOIN
+  delivery.Orders_and_Dishes od ON o.order_id = od.order_id
+WHERE 
+  od.dish_id IN (
+    SELECT
+      dish_id 
+    FROM
+      delivery.Dishes 
+    WHERE
+      dish_price > 450
+  )
 ORDER BY
-  avg_dish_weight DESC;
-
--- Запрос на вывод средней зарплаты по каждой профессии:
-SELECT 
-  p.profession_name,
-  AVG(p.profession_salary) AS avg_salary
-FROM 
-  delivery.Professions p
-JOIN 
-  delivery.Staff s ON p.profession_id = s.profession_id
-GROUP BY 
-  p.profession_name;
+  client_id;
