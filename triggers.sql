18290f69b2e6acce0a10bed273eb5ec56b345cb8
weeetttyyyou <113246759+weeetttyyyou@users.noreply.github.com>
2024-05-14 21:59:14 +0300

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -1,23 +1,48 @@
--- Функция, обновляющая рейтинг ресторана:
-CREATE OR REPLACE FUNCTION update_restaurant_rating()
+-- Функция, обновляющая общую стоимость заказа
+CREATE OR REPLACE FUNCTION UpdateTotalCost()
+RETURNS TRIGGER AS $$
+BEGIN
+  UPDATE delivery.Orders
+  SET order_total_cost = (
+    SELECT SUM(dish_price)
+    FROM delivery.Orders_and_Dishes
+    JOIN delivery.Dishes ON delivery.Orders_and_Dishes.dish_id = delivery.Dishes.dish_id
+    WHERE order_id = NEW.order_id
+  )
+  WHERE order_id = NEW.order_id;
+
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- Триггер, вызывающий функцию UpdateTotalCost
+CREATE OR REPLACE TRIGGER UpdateTotalCostTrigger
+AFTER INSERT OR UPDATE OR DELETE ON delivery.Orders_and_Dishes
+FOR EACH ROW
+EXECUTE FUNCTION UpdateTotalCost();
+
+
+-- Функция, обновляющая меню ресторана:
+CREATE OR REPLACE FUNCTION UpdateRestaurantComment()
 RETURNS TRIGGER AS $$
 DECLARE
-  new_restaurant_rating INTEGER;
+  new_restaurant_comment VARCHAR(255);
 BEGIN
-  SELECT r.restaurant_rating
-  INTO new_restaurant_rating
-  FROM delivery.Restaurant r
-  JOIN delivery.Restaurant_and_Dishes rd ON r.restaurant_id = rd.restaurant_id
-  JOIN delivery.Dishes d ON rd.dish_id = d.dish_id
+  SELECT r.restaurant_comment
+  INTO new_restaurant_comment
+  FROM delivery.Restaurants r
+  JOIN delivery.Dishes_and_Restaurants dr ON r.restaurant_id = dr.restaurant_id
+  JOIN delivery.Dishes d ON dr.dish_id = d.dish_id
   JOIN delivery.Orders_and_Dishes od ON d.dish_id = od.dish_id
   WHERE od.order_id = NEW.order_id;
 
   UPDATE delivery.Restaurants
-  SET restaurant_rating = new_restaurant_rating
+  SET restaurant_comment = new_restaurant_comment
   WHERE restaurant_id IN (
     SELECT rd.restaurant_id
-    FROM delivery.Restaurant_and_Dishes rd
-    JOIN delivery.Orders_and_Dishes od ON rd.dish_id = od.dish_id
+    FROM delivery.Dishes_and_Restaurants dr
+    JOIN delivery.Orders_and_Dishes od ON dr.dish_id = od.dish_id
     WHERE od.order_id = NEW.order_id
   );
 
@@ -25,8 +50,8 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- Триггер, вызывающий функцию update_restaurant_rating():
-CREATE TRIGGER update_restaurant_rating_trigger
+-- Триггер, вызывающий функцию UpdateRestaurantComment:
+CREATE TRIGGER UpdateRestaurantCommentTrigger
 AFTER INSERT OR UPDATE OR DELETE ON delivery.Orders_and_Dishes
 FOR EACH ROW
-EXECUTE FUNCTION update_restaurant_rating();
+EXECUTE FUNCTION UpdateRestaurantComment();
